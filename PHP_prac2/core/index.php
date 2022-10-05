4fe08ee8774637593ae369ba8c0236a8aa4ab066
MycroftBrooks <57303529+MycroftBrooks@users.noreply.github.com>
2022-10-05 19:55:18 +0300

    second prac

---
+++ PHP_prac2/core/index.php
@@ -0,0 +1,23 @@
+<html lang="ru">
+<head>
+    <title>Main Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+<body>
+    <div class="d-flex flex-column min-vh-100 justify-content-center align-items-center">
+        <h3>Select function</h3>
+        <div class="row row-cols-auto">
+            <div class="col">
+                <a href="/console.php" class="btn btn-primary">Console</a>
+            </div>
+            <div class="col">
+                <a href="/sort.php" class="btn btn-primary">Sort</a>
+            </div>
+            <div class="col">
+                <a href="/drawer.php" class="btn btn-primary">Drawer</a>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+</body>
+</html>
